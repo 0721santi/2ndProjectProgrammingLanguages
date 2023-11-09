e65d6c07dd643359a298ad35494f0375774860fa
Santiago Idarraga Ceballos <san200526@gmail.com>
2023-11-09 13:22:04 -0500

    Firsts functions

---
+++ ISBNchecker.hs
@@ -0,0 +1,13 @@
+verifySpacing isbn
+  | ' ' `elem` isbn = 1
+  | '-' `elem` isbn = 2
+  | otherwise = 0
+
+removeSpaces isbn spaced
+  | spaced == 1 = filter (/= ' ') isbn
+  | spaced == 0 = filter (/= '-') isbn
+  | otherwise = isbn
+
+-- checkLastDigit isbn
+--     if(last isbn == 'X')
+--         then init isbn
