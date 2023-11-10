1bc2a768ef8d5e6c7c21d6ca2549ad1447f6b7ad
Santiago Idarraga Ceballos <san200526@gmail.com>
2023-11-10 09:02:58 -0500

    Función que devuelve el ISBN final.

--- ISBNchecker.hs
+++ ISBNchecker.hs
@@ -1,16 +1,19 @@
 verifySpacing isbn
-  | ' ' `elem` isbn = 1
-  | '-' `elem` isbn = 2
-  | otherwise = 0
+    | ' ' `elem` isbn = 1
+    | '-' `elem` isbn = 2
+    | otherwise = 0
 
 removeSpaces isbn spaced
-  | spaced == 1 = filter (/= ' ') isbn
-  | spaced == 2 = filter (/= '-') isbn
-  | otherwise = isbn
-  
+    | spaced == 1 = filter (/= ' ') isbn
+    | spaced == 2 = filter (/= '-') isbn
+    | otherwise = isbn
+
 
 generaISBN isbn = removeSpaces isbn (verifySpacing isbn)
 
--- checkLastDigit isbn
---     if(last isbn == 'X')
---         then init isbn
+add10 isbn = isbn ++ "10"
+
+checkLastDigit isbn
+    | last isbn == 'X' = add10 (init isbn)
+
+finalISBN isbn = checkLastDigit (generaISBN isbn)
